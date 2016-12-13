199ae7ebd32fab9ce4126a65a126344f3c617801
Lyu, Yun-Huan <luyunhuan@gmail.com>
2016-12-14 00:02:31 +0800

    modified linear_classifier for softMax
    notice the def loss in BIG LinearClassifier class

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -11,7 +11,6 @@ class LinearClassifier(object):
             batch_size=200, verbose=False):
     """
     Train this linear classifier using stochastic gradient descent.
-
     Inputs:
     - X: A numpy array of shape (N, D) containing training data; there are N
       training samples each of dimension D.
@@ -22,7 +21,6 @@ class LinearClassifier(object):
     - num_iters: (integer) number of steps to take when optimizing
     - batch_size: (integer) number of training examples to use at each step.
     - verbose: (boolean) If true, print progress during optimization.
-
     Outputs:
     A list containing the value of the loss function at each training iteration.
     """
@@ -68,7 +66,7 @@ class LinearClassifier(object):
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
       
-      self.W-=grad*loss*1e-8                                              #upgrade the WEIGHT, here I didn't use "learning rate" to avoid 
+      self.W-=grad*loss*learning_rate*0.001                               #upgrade the WEIGHT, here I didn't use "learning rate" to avoid 
                                                                           #NaN, it shall have something wrong here, check later.
       #pass
       #########################################################################
@@ -84,10 +82,8 @@ class LinearClassifier(object):
     """
     Use the trained weights of this linear classifier to predict labels for
     data points.
-
     Inputs:
     - X: D x N array of training data. Each column is a D-dimensional point.
-
     Returns:
     - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
       array of length N, and each element is an integer giving the predicted
@@ -121,20 +117,19 @@ class LinearClassifier(object):
     """
     Compute the loss function and its derivative. 
     Subclasses will override this.
-
     Inputs:
     - X_batch: A numpy array of shape (N, D) containing a minibatch of N
       data points; each point has dimension D.
     - y_batch: A numpy array of shape (N,) containing labels for the minibatch.
     - reg: (float) regularization strength.
-
     Returns: A tuple containing:
     - loss as a single float
     - gradient with respect to self.W; an array of the same shape as W
     """
     #pass
-    return svm_loss_vectorized(self.W, X_batch, y_batch, reg) # utilize the svm_loss_vectorized function to return the loss and gradient
-
+    #return svm_loss_vectorized(self.W, X_batch, y_batch, reg) # utilize the svm_loss_vectorized function to return the loss and gradient
+    return softmax_loss_vectorized(self.W, X_batch, y_batch, reg)
+    
 class LinearSVM(LinearClassifier):
   """ A subclass that uses the Multiclass SVM loss function """
 
